23f3d25d1c49726fe700f86e68c54a580aef13ae
Will Thomas Yoder <yoderw@c097h234.acad.reed.edu>
2017-02-07 15:02:39 -0800

    hw3.py

--- hw/hw3/hw3.py
+++ hw/hw3/hw3.py
@@ -29,22 +29,33 @@ def fib3(n):
 		c, b, a = c + b + a, c, b
 	return c
 
-"""
-  *
- * *
-* * *
-
-'  *\n * *\n* * *\n'
-
-    *
-   * *
-  * * *
- * * * *
-* * * * *
-"""
-
 def pyramid(h):
 	string = ''
 	for i in range(h):
 		string += (" " * (h - (i + 1)) + ("* " * i) + "*" + "\n")
 	return string
+
+def collatz(n):
+
+	def seq_len(m):
+		i = 1
+		while m != 1:
+			m = m // 2 if m % 2 == 0 else (m * 3) + 1
+			i += 1
+		return i
+
+	i = 1
+	while True:
+		if seq_len(i) >= n:
+			return i
+		else:
+			i += 1
+
+def digit(x, n):
+	return 0 if (10 ** n) > x else (x % (10 ** (n + 1))) // (10 ** n)
+
+def reverse(n):
+	for i in range
+
+def numpal(n):
+	return n == reverse(n)
